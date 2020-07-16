6da87011297e448de9184d484b0374fd09632dac
SirWindfield <SirWindfield@users.noreply.github.com>
2020-07-16 16:22:53 +0000

    deploy: 502e4f2d45490d5e451cf1d83259d6699c131c00

---
+++ search-index.js
@@ -0,0 +1,4 @@
+var searchIndex = JSON.parse('{\
+"zerotask_rust_lib_template":{"doc":"","i":[[5,"main","zerotask_rust_lib_template","",null,[[]]]],"p":[]}\
+}');
+addSearchOptions(searchIndex);initSearch(searchIndex);
---
+++ source-files.js
@@ -0,0 +1,3 @@
+var N = null;var sourcesIndex = {};
+sourcesIndex["zerotask_rust_lib_template"] = {"name":"","files":["main.rs"]};
+createSourceSidebar();
